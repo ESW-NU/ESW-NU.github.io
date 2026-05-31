71bb87ae765ad8b20f0059c6a5036ae95ab35aa1
Gabriela Avila <GabrielaAvila2028@u.northwestern.edu>
2026-05-31 15:30:49 -0500

    Create PLAsticCarousel.js

---
+++ layout/scripts/PLAsticCarousel.js
@@ -0,0 +1,19 @@
+// The following .js file holds the function for switching between images for PLAstic
+
+const images = [
+    "../images/PLAstic images/Working on Hopper.jpg",
+    "../images/PLAstic images/Yuna cutting Hopper.HEIC",
+    "../images/PLAstic images/Group Blender Testing.jpg",
+    "../images/PLAstic images/Alex on Bandsaw.HEIC",
+    "../images/PLAstic images/PLAstic in Shop.HEIC"
+];
+
+let currentIndex = 0;
+
+function changeImage(direction) {
+    currentIndex =
+        (currentIndex + direction + images.length) % images.length;
+
+    document.getElementById("carousel-image").src =
+        images[currentIndex];
+}
